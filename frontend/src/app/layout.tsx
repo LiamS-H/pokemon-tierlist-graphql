946fb54276e2195f962bac74bdf5113b579b3211
liamsh <liamsh@gmail.com>
2025-02-15 15:31:38 -0800

    add navbar

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "./providers";
+import { NavBar } from "./navbar";
 
 export const metadata: Metadata = {
     title: "Pokemon Tierlist Maker",
@@ -15,7 +16,10 @@ export default function RootLayout({
     return (
         <html lang="en">
             <body className={`antialiased`}>
-                <Providers>{children}</Providers>
+                <Providers>
+                    <NavBar />
+                    {children}
+                </Providers>
             </body>
         </html>
     );
---
+++ frontend/src/app/navbar.tsx
@@ -0,0 +1,40 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { Url } from "next/dist/shared/lib/router/router";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import type { ReactNode } from "react";
+
+function NavbarItem({ href, children }: { href: Url; children: ReactNode }) {
+    const path = usePathname();
+    const active =
+        href === "/" ? path === "/" : path.startsWith(href.toString());
+    return (
+        <li>
+            <Link href={href}>
+                <Button
+                    variant={active ? "default" : "ghost"}
+                    className={
+                        active
+                            ? "underline underline-offset-2"
+                            : "text-primary-foreground"
+                    }
+                >
+                    {children}
+                </Button>
+            </Link>
+        </li>
+    );
+}
+
+export function NavBar() {
+    return (
+        <div className="sticky top-0 p-2 bg-primary">
+            <ul className="flex gap-4">
+                <NavbarItem href={"/"}>Create</NavbarItem>
+                <NavbarItem href={"/tierlists"}>Tierlists</NavbarItem>
+                <NavbarItem href={"/pokemon"}>Pokemon</NavbarItem>
+            </ul>
+        </div>
+    );
+}
