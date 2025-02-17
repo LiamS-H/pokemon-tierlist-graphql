79b41814bc8e528f9379de06a9f2ab45e464f5a2
liamsh <liamsh@gmail.com>
2025-02-17 10:57:43 -0800

    feat sonner toaster

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Providers from "./providers";
 import { NavBar } from "./navbar";
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
     title: "Pokemon Tierlist Maker",
@@ -17,6 +18,7 @@ export default function RootLayout({
         <html lang="en" className="h-screen">
             <body className={`antialiased h-full flex flex-col`}>
                 <Providers>
+                    <Toaster />
                     <NavBar />
                     {children}
                 </Providers>
